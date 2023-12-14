9ac87a4a13524d159aaa55022e9b18294db51756
Srikar Dhulipalla <srikar.dhulipalla@hotmail.com>
2023-12-15 00:16:30 +0100

    Init Commit

---
+++ mvno-admin/App_Start/RouteConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace mvno_admin
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "App", action = "Dashboards", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Dashboard",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "App", action = "Dashboards", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Tools",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "App", action = "Tools", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Subscribers",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Subscribers", action = "SubscribersList", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Create_New_Subscriber",
+                url: "app/{controller}/{action}/{id}",
+                defaults: new { controller = "Subscribers", action = "CreateNewSubscriber", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ mvno-admin/Controllers/SubscribersController.cs
@@ -0,0 +1,76 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using mvno_admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvno_admin.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        public ActionResult SubscribersList()
+        {
+            ViewBag.Message = "Launched Subscribers Page";
+            return View("~/Views/App/Subscribers/Index.cshtml");
+
+        }
+
+        public ActionResult CreateNewSubscriber()
+        {
+            ViewBag.Message = "Launched Create Subscriber Page";
+            return View("~/Views/App/Subscribers/Add/Index.cshtml");
+
+        }
+
+        public ActionResult GetSubscribersList([DataSourceRequest] DataSourceRequest request)
+        {
+            try
+            {
+                List<Subscribers> _subscriber = new List<Subscribers>();
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+                _subscriber.Add(new Subscribers(35684836, "Active", "Demo", "unavosupport", 9203052333, "John", "Clavin", "3,000 Data Only Pan", 123456, "ACP", false, false));
+
+                DataSourceResult result = _subscriber.ToDataSourceResult(request);
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
+}
